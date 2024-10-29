f200b79149bfb3580c41c4eb2559d0dbb0ae36fd
Estêvão Calera <estevaoscalera@gmail.com>
2024-10-29 01:16:11 -0300

    2501. runtime 1623ms and memory 75.5MB

--- 2501. Longest Square Streak in an Array.ts
+++ 2501. Longest Square Streak in an Array.ts
@@ -1,24 +1,24 @@
 export function longestSquareStreak(nums: number[]): number {
   nums = nums.sort((a, b) => b - a);
   nums = [...new Set(nums)];
-  let maxStreak: number[] = [];
-  while (nums.length > maxStreak.length) {
-    let streak = [nums[0]];
+  let maxStreak = 0;
+  while (nums.length > maxStreak) {
+    let streak = 1;
     let nextSquareRoot = findSquareRoot(nums[0], nums);
     nums.splice(0, 1);
 
     while (nextSquareRoot) {
-      streak.push(nextSquareRoot);
+      streak++;
       nums.splice(nums.indexOf(nextSquareRoot), 1);
       nextSquareRoot = findSquareRoot(nextSquareRoot, nums);
     }
 
-    if (streak.length > maxStreak?.length) {
+    if (streak > maxStreak) {
       maxStreak = streak;
     }
   }
 
-  return maxStreak.length > 1 ? maxStreak.length : -1;
+  return maxStreak > 1 ? maxStreak : -1;
 };
 
 function findSquareRoot(num: number, nums: number[]): number | undefined {
