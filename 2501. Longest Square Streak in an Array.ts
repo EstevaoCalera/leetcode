97433ec585e06ec463f7c32095e1ed7d0c54983b
Estêvão Calera <estevaoscalera@gmail.com>
2024-10-29 16:00:14 -0300

    after studying set structure, 39ms and 69.4MB

--- 2501. Longest Square Streak in an Array.ts
+++ 2501. Longest Square Streak in an Array.ts
@@ -1,34 +1,14 @@
 export function longestSquareStreak(nums: number[]): number {
-  nums = nums.sort((a, b) => b - a);
-  nums = [...new Set(nums)];
-  let maxStreak = 0;
-  while (nums.length > maxStreak) {
+  const set = new Set(nums);
+  let maxStreak = 1;
+  for (let item of set) {
     let streak = 1;
-    let nextSquareRoot = findSquareRoot(nums[0], nums);
-    nums.splice(0, 1);
-
-    while (nextSquareRoot) {
+    let i = item;
+    while (set.has(i * i)) {
+      i = i * i;
       streak++;
-      nums.splice(nums.indexOf(nextSquareRoot), 1);
-      nextSquareRoot = findSquareRoot(nextSquareRoot, nums);
     }
-
-    if (streak > maxStreak) {
-      maxStreak = streak;
-    }
+    maxStreak = Math.max(maxStreak, streak);
   }
-
   return maxStreak > 1 ? maxStreak : -1;
 };
-
-function findSquareRoot(num: number, nums: number[]): number | undefined {
-  const sqrtNum = Math.sqrt(num)
-  if (!isInt(sqrtNum)) {
-    return;
-  }
-  return nums.find(n => n === sqrtNum);
-}
-
-function isInt(x: number): boolean {
-  return (x | 0) === x;
-}
